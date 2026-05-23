1ce0e8585be23560ab28ae8e7e2f74c8e007488e
nagnath <nagnathsomanwar@example.com>
2026-05-23 15:17:33 +0530

    retring deployement 2

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello from GitHub Actions CI/CD")  
+    return HttpResponse("Hello from GitHub Actions CI/CD")
